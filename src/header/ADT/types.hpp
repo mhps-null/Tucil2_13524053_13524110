37458fab5e2017e672953663782a5136f7cd83d1
Jennifer Khang <jenniferkhang07@gmail.com>
2026-03-20 14:51:21 +0700

    feat: init object viewer

--- src/header/ADT/types.hpp
+++ src/header/ADT/types.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+/*
+ * ADT untuk menyimpan data vertex, face, dan bounding box pada mesh.
+ */
+
 struct Vertex
 {
     float x, y, z;
@@ -13,4 +17,20 @@ struct Face
 struct BoundingBox
 {
     Vertex min, max;
+};
+
+
+/*
+ *  ADT untuk Object Viewer (Camera) sederhana.
+ */
+
+struct Camera {
+   double x, y, z;
+   double angleX, angleY, angleZ;
+};
+
+struct ScreenData {
+   int x_screenPositionOfPt, y_screenPositionOfPt;
+   double zFar = 100;
+   int width=640, height=480;
 };
---
+++ src/objectviewer/CameraLogic.cpp
@@ -0,0 +1,4 @@
+/*
+ * CameraLogic.cpp: berisi implementasi fungsi-fungsi untuk mengatur logika kamera pada Object Viewer
+ * [On Progress]
+ */
